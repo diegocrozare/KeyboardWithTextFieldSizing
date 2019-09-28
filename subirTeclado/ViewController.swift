bcc75bf330181d42f20c9d1b9847543ea8fd53a5
Diego William Crozare <s2it_dcrozare@uolinc.com>
2019-09-27 23:42:48 -0300

    Implementation

--- subirTeclado/ViewController.swift
+++ subirTeclado/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        subKeyboard()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unshowKeyboard()
+    }
+    
+    // Create notification to warn you when the keyboard goes up and down
+    func subKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardHideShow(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // Remove the Notifications
+    func unshowKeyboard() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // Delegate TextField - click return on Keyboard warning
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Open keyboard textField sizing
+    @objc func keyBoardWillShow(_ notification: Notification) {
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
 
+    // Close keyboard textField sizing
+    @objc func keyBoardHideShow(_ notification: Notification) {
+        view.frame.origin.y += getKeyboardHeight(notification)
+    }
+    
+    // Height keyboard
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
 }
-
